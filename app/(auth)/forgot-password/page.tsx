b5fe228317f3369141ab8b2f03ceff5f75acd53a
MarkLumaino <marklumaino@gmail.com>
2025-12-11 10:50:54 +0800

    forgot password

---
+++ app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+export default function ForgotPasswordPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-4">Forgot Password</h1>
+      <p className="text-slate-600">
+        Lorem epsel
+      </p>
+    </div>
+  )
+}
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,9 +1,9 @@
 export default function UserPage() {
   return (
     <div>
-      <h1 className="text-2xl font-semibold mb-4">Dashboard</h1>
+      <h1 className="text-2xl font-semibold mb-4">Users</h1>
       <p className="text-slate-600">
-        This sidebar only appears in dashboard pages.
+        Lorem epsel
       </p>
     </div>
   )
